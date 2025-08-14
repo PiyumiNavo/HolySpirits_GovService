f84de9f13adca59b7d79979ecc8de708e85527ff
kalpa madhushan <kalpamadhushan18@gmail.com>
2025-08-15 00:30:55 +0530

    add MulterModule to FilesModule and ScheduleModule to SchedulerModule

--- backend/src/files/files.module.ts
+++ backend/src/files/files.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
+  imports: [
+    MulterModule.register({
+      dest: './uploads',
+    }),
+  ],
   providers: [FilesService],
   controllers: [FilesController],
 })
