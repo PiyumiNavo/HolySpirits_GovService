a142ccb88cac729a9066514eaa551b11ce8ce17e
kalpa madhushan <kalpamadhushan18@gmail.com>
2025-08-15 00:26:55 +0530

    add necessary modules and libraries

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DepartmentsModule } from './departments/departments.module';
+import { GovServicesModule } from './gov-services/gov-services.module';
+import { FilesModule } from './files/files.module';
+import { MailModule } from './mail/mail.module';
+import { SchedulerModule } from './scheduler/scheduler.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // so we don't have to import everywhere
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_URI'),
+      }),
+    }),
+    AuthModule,
+    UsersModule,
+    DepartmentsModule,
+    GovServicesModule,
+    FilesModule,
+    MailModule,
+    SchedulerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/gov-services/gov-services.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('gov-services')
+export class GovServicesController {}
